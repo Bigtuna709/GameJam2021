b52f080970100ee6638479685dcd03041f5f7c91
Merik <m.ally94@yahoo.ca>
2021-10-04 15:19:05 -0400

    Art Test Assets & PlayerMovement Script
    Created a PlayerMovement Script and imported some art test assets.

---
+++ GameJam2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
