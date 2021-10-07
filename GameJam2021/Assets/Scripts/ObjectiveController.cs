ae7167211707a0adad9b43f9d1b3d80dd72bbf9e
Merik <m.ally94@yahoo.ca>
2021-10-07 16:56:11 -0400

    Obj Animation & Script

--- GameJam2021/Assets/Scripts/ObjectiveController.cs
+++ GameJam2021/Assets/Scripts/ObjectiveController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class ObjectiveController : MonoBehaviour
 {
     public GameObject sound;
+    public Animation animator;
+
+    public void Awake()
+    {
+        animator.Play("MicToy_a");    
+    }
 
     public void Collect()
     {
