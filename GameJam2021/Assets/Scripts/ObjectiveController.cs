74622032193f1e11167304d5ace1d9fdd576ad84
PhasedChaos <chris_clark@bell.net>
2021-10-07 15:52:02 -0400

    Collection sound

--- GameJam2021/Assets/Scripts/ObjectiveController.cs
+++ GameJam2021/Assets/Scripts/ObjectiveController.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class ObjectiveController : MonoBehaviour
 {
+    public GameObject sound;
+
     public void Collect()
     {
         gameObject.SetActive(false);
         GameManager.Instance.CollectObjective();
+        sound.GetComponent<AudioSource>().Play();
     }
 }
