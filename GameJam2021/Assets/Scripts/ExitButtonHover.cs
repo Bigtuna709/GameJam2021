900ef508948c9e4fe9bedab6097ddd5e52b236b1
AnthonyDeFazio <anthonydefazio@rogers.com>
2021-10-07 16:18:10 -0400

    Changed Title Screen Scene
    Added button animations to title screen

--- GameJam2021/Assets/Scripts/ExitButtonHover.cs
+++ GameJam2021/Assets/Scripts/ExitButtonHover.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ExitButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public RectTransform Button;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button.GetComponent<Animator>().Play("BtIdle");
+        Button.GetComponent<Animator>().Play("ExitBtIdle");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Button.GetComponent<Animator>().Play("PlayBtHover");
+        Button.GetComponent<Animator>().Play("ExitBtHover");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Button.GetComponent<Animator>().Play("BtIdle");
+        Button.GetComponent<Animator>().Play("ExitBtIdle");
     }
-    
+
 }
