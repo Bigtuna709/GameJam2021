b1be456664f9b63f5871f39bdc1f00ee79463091
Merik <m.ally94@yahoo.ca>
2021-10-08 03:23:19 -0400

    Started on First Level

--- GameJam2021/Assets/Scripts/PlayerMovement.cs
+++ GameJam2021/Assets/Scripts/PlayerMovement.cs
@@ -59,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
     private IEnumerator IdleState()
     {
         SetCharacterState("Idle");
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         if (_rigidbody.velocity.y < 0.1f )
         {
             SetCharacterState("Idle2");
