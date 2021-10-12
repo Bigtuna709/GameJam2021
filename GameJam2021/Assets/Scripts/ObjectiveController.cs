fbde1acea54412934c9e1a6430aa518058eff818
Merik <m.ally94@yahoo.ca>
2021-10-12 01:54:00 -0400

    Created Level 3 & Fixed Scene Issues

--- GameJam2021/Assets/Scripts/ObjectiveController.cs
+++ GameJam2021/Assets/Scripts/ObjectiveController.cs
@@ -5,15 +5,10 @@ public class ObjectiveController : MonoBehaviour
     public GameObject sound;
     public Animator animator;
 
-    public void Awake()
-    {
-        //animator.Play("MicToy_a");
-    }
-
     public void Collect()
     {
+        sound.GetComponent<AudioSource>().Play();
         gameObject.SetActive(false);
         GameManager.Instance.CollectObjective();
-        sound.GetComponent<AudioSource>().Play();
     }
 }
